fa83aead6aa6bfdc31a266b0af955b77289e0e13
Jung-Ho Um <jhum@kisti.re.kr>
2025-08-14 14:22:33 +0900

    build config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/sunujh6/vibecode-omok',
   plugins: [react()],
+  build: { outDir: 'dist', sourcemap: true },
 })
