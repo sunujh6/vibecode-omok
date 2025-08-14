e1930aedaa78accfc3d021035ff3360c9a1f9de9
Jung-Ho Um <jhum@kisti.re.kr>
2025-08-14 14:51:32 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/sunujh6/vibecode-omok',
+  base: 'https://sunujh6.github.io/vibecode-omok/',
   plugins: [react()],
   build: { outDir: 'dist', sourcemap: true },
 })
